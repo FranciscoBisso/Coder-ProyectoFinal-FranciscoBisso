d10c5f786e0bd3091f835de4a1f5d58597eed17b
FranciscoBisso <fjbisso@gmail.com>
2022-09-28 19:45:27 -0300

    App/tests.py: agrego test de vistas publicas (no requieren login). Todo ok

--- App/tests.py
+++ App/tests.py
@@ -4,8 +4,8 @@ import datetime
 from django.contrib.auth.models import User
 from App.models import Post, Comment
 
-# test_creation_of_post_and_comment ---> Ran 1 test in 0.002s OK
 
+# TEST DE CREACIÓN DE USUARIOS, POST Y COMENTARIOS DE POST.
 class AppTestCase(TestCase):
     def setUp(self):
         User.objects.create(
@@ -33,3 +33,27 @@ class AppTestCase(TestCase):
         p2 = Comment.objects.get(comment='Lorem ipsum dolor')
         self.assertEqual(p1.title, 'Title')
         self.assertEqual(p2.comment, 'Lorem ipsum dolor')
+# RESULTADO test_creation_of_post_and_comment: Ran 1 test in 0.002s OK
+
+
+
+# TEST DE VISTAS ACCESIBLES POR TODOS
+class ViewTests(TestCase):
+    def test_public_views(self):
+        home = self.client.get(reverse('Home'))
+        self.assertEqual(home.status_code, 200)
+        self.assertContains(home, '')
+
+        login = self.client.get(reverse('Login'))
+        self.assertEqual(login.status_code, 200)
+        self.assertContains(login, '')
+
+        register = self.client.get(reverse('Register'))
+        self.assertEqual(register.status_code, 200)
+        self.assertContains(register, '')
+        
+        about_me = self.client.get(reverse('AboutMe'))
+        self.assertEqual(about_me.status_code, 200)
+        self.assertContains(about_me, '')
+# RESULTADO test_public_views: Ran 2 tests in 0.025s OK
+
